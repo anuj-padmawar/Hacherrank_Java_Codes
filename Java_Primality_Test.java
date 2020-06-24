0ba1640215ff4fba6d1030bd9c62e01f5da78415
Anuj Padmawar <35773688+anuj-padmawar@users.noreply.github.com>
2020-06-24 15:09:28 +0530

    Add files via upload

---
+++ Java_Primality_Test.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner sc= new Scanner(System.in);
+
+    public static void main(String[] args) {
+        BigInteger n=sc.nextBigInteger();
+        System.out.println(n.isProbablePrime(100) ? "prime" : "not prime");
+
+        sc.close();
+    }
+}
+
