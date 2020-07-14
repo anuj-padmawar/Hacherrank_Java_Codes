d274f7f26dc1adc4846d863815d43213417cadc3
Anuj Padmawar <35773688+anuj-padmawar@users.noreply.github.com>
2020-07-14 11:37:43 +0530

    Add files via upload

---
+++ Java_MD5.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Solution {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        sc.close();
+        
+        
+        MessageDigest md=MessageDigest.getInstance("MD5");
+        md.update(str.getBytes());
+        byte[] digest = md.digest();
+        
+        //value in hexadecimal 
+        for (byte b : digest) 
+        {
+            System.out.format("%02x", b);
+        }
+    }
+}
